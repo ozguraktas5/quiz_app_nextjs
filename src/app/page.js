b95fce6285923222de1052622289ca0dd145d80d
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-03-18 12:29:59 +0300

    title

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="wrapper flex justify-center items-center h-screen">
-      <div className="bg-white p-3 rounded-md md:w-[90%] lg:w-[70%] w-full h-full md:h-[70%] lg:h-[70%] shadow-lg max-w-4xl">Welcome to Quiz App</div>
+      <div className="bg-white p-3 rounded-md md:w-[90%] lg:w-[70%] w-full h-full md:h-[70%] lg:h-[70%] shadow-lg max-w-4xl">
+        <h1 className="text-center p-2 text-blue-600 text-3xl tracking-wider font-bold">Welcome to QuizQuest</h1>
+        <div className="w-full h-[2px] bg-border bg-slate-200"></div>
+      </div>
     </main>
     
   );
