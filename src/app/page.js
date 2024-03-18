e2274036e320c77d34c824c0d30e1beb3355cae8
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-03-18 13:06:18 +0300

    add png

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,12 @@ export default function Home() {
       <div className="bg-white p-3 rounded-md md:w-[90%] lg:w-[70%] w-full h-full md:h-[70%] lg:h-[70%] shadow-lg max-w-4xl">
         <h1 className="text-center p-2 text-blue-600 text-3xl tracking-wider font-bold">Welcome to QuizQuest</h1>
         <div className="w-full h-[2px] bg-border bg-slate-200"></div>
+        <div className="grid grid-cols-1 md:grid-cols-2 p-4 md:p-9 gap-3">
+          <div className="h-full relative">
+            <Image src="/quiz.png" width="350" height="350" className="object-cover object-center vertical-middle" />
+          </div>
+          <div></div>
+        </div>
       </div>
     </main>
     
