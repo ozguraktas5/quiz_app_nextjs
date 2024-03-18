baaacfc9e6f753bd998ef29f7a14cf502c046797
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-03-18 12:01:43 +0300

    visual part

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen">Welcome to Quiz App</div>
+    <main className="wrapper flex justify-center items-center h-screen">
+      <div className="bg-white p-3 rounded-md">Welcome to Quiz App</div>
+    </main>
+    
   );
 }
