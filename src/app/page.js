7381758073b9500f548d658cc979b202faedc3aa
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-03-18 12:15:41 +0300

    responsive design

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="wrapper flex justify-center items-center h-screen">
-      <div className="bg-white p-3 rounded-md">Welcome to Quiz App</div>
+      <div className="bg-white p-3 rounded-md md:w-[90%] lg:w-[70%] w-full h-full md:h-[70%] lg:h-[70%] shadow-lg max-w-4xl">Welcome to Quiz App</div>
     </main>
     
   );
