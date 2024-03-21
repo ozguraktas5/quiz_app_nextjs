07d045b4ea6b7684b1ca1c32dfe61ddd351a199d
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-03-21 15:28:25 +0300

    edit

--- src/pages/hello.tsx
+++ src/pages/hello.tsx
@@ -1,10 +1,4 @@
 import React from "react";
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Hello',
-};
-
 export default function HelloPage() {
   return (
     <main>Hello</main>
