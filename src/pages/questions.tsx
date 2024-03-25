b1ea7d4cc4604b0f1e0b0307f054e318c0c93b85
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-03-25 21:12:19 +0300

    questions.tsx page

--- src/pages/questions.tsx
+++ src/pages/questions.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import "../app/globals.css";
 export default function QuestionsPage() {
   return (
-    <main>Questions</main>
+    <main className="wrapper flex justify-center items-center h-screen">
+      <div className="bg-white p-3 rounded-md md:w-[90%] w-full h-full md:h-[90%] lg:h-[90%] shadow-lg max-w-4xl">
+        <h1 className="text-center p-2 text-blue-600 text-5xl tracking-wider font-bold">
+          Quizy
+        </h1>
+        <div className="w-full h-[2px] bg-border bg-slate-200 mt-1"></div>
+        Questions
+      </div>
+    </main>
   );
 }
