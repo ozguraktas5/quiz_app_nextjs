28a6ab022ed87c5fe9021e3dd664f7841465fa70
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-03-25 21:22:19 +0300

    questions.tsx page

--- src/pages/questions.tsx
+++ src/pages/questions.tsx
@@ -1,5 +1,42 @@
 import React from "react";
 import "../app/globals.css";
+import { Stack } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import CircularProgress from "@mui/material/CircularProgress";
+import LinearProgress, {
+  linearProgressClasses,
+} from "@mui/material/LinearProgress";
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+  height: 15,
+  borderRadius: 5,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor:
+      theme.palette.grey[theme.palette.mode === "light" ? 200 : 800],
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 5,
+    backgroundColor: theme.palette.mode === "light" ? "#1a90ff" : "#308fe8",
+  },
+}));
+
+function GradientCircularProgress() {
+  return (
+    <React.Fragment>
+      <svg width={0} height={0}>
+        <defs>
+          <linearGradient id="my_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+            <stop offset="0%" stopColor="#e01cd5" />
+            <stop offset="100%" stopColor="#1CB5E0" />
+          </linearGradient>
+        </defs>
+      </svg>
+      <CircularProgress
+        sx={{ "svg circle": { stroke: "url(#my_gradient)" } }}
+      />
+    </React.Fragment>
+  );
+}
 export default function QuestionsPage() {
   return (
     <main className="wrapper flex justify-center items-center h-screen">
@@ -9,6 +46,9 @@ export default function QuestionsPage() {
         </h1>
         <div className="w-full h-[2px] bg-border bg-slate-200 mt-1"></div>
         Questions
+        <Stack spacing={2} sx={{ flexGrow: 1 }}>
+          <BorderLinearProgress variant="determinate" value={50} />
+        </Stack>
       </div>
     </main>
   );
